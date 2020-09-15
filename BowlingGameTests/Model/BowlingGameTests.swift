1ab35b297ae47d9c8636cf70c2208f6b9821e948
trial <trial6151@users.noreply.github.com>
2020-09-15 23:14:35 +0200

    Added test for when player did not score in any round should return score as 0

--- BowlingGameTests/Model/BowlingGameTests.swift
+++ BowlingGameTests/Model/BowlingGameTests.swift
@@ -31,4 +31,28 @@ class BowlingGameTests: XCTestCase {
         XCTAssertTrue(finalScore >= 0, "Game should have valid score.")
     }
 
+    func test_whenPlayerDidNotScoreInAllDeliveriesOfGame_shouldReturnScore0() {
+        //Arrange
+        self.makeNormalRounds(10, 0, 0)
+
+        //Act
+        let finalScore = self.sut.finalScore
+
+        //Assert
+        XCTAssertEqual(finalScore, 0, "Score of the game should have been 0.")
+    }
+
 }
+
+//MARK:- Stub helpers
+
+private extension BowlingGameTests {
+
+    func makeNormalRounds(_ count: Int,_ firstDelivery: Int,_ secondDelivery: Int) {
+        for _ in 0..<count {
+            self.sut.normalRound(firstDelivery, secondDelivery)
+        }
+    }
+
+}
+
